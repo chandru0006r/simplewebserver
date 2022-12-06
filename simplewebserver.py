8c0a98378d44342bc0589235baf59ba8fda4b8cb
chandru0006r <charanchandru587@gmail.com>
2022-12-06 13:39:16 +0530

    working commit

--- simplewebserver.py
+++ simplewebserver.py
@@ -6,7 +6,14 @@ content = """
 <title>My webserver</title>
 </head>
 <body>
-<h1>Welcome</h1>
+<h1>Top five web application development frameworks.</h1>
+<ol style="font-size:25px;">
+<li>React JS</li>
+<li>Django</li>
+<li>Node JS</li>
+<li>Larvarel</li>
+<li>Angular JS</li>
+</ol>
 </body>
 </html>
 """
@@ -15,9 +22,10 @@ class myhandler(BaseHTTPRequestHandler):
         print("request received")
         self.send_response(200)
         self.send_header('content-type', 'text/html; charset=utf-8')
-        self.wfile.write(content.encode())
         self.end_headers()
+        self.wfile.write(content.encode())
 server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
+#python3 simplewebserver.py
